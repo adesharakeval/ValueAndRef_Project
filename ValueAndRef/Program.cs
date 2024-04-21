d3982b8fcb3d61d5abea1f667bc90b8f048b9065
adesharakeval <adesharakeval@gmail.com>
2024-04-21 15:06:56 +0530

    code commit

---
+++ ValueAndRef/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueAndRef
+{
+    class Cat
+    {
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            #region Boxing
+           
+            string Box = "Watch";
+        
+            object Giftwrapper = Box;
+            
+            Console.WriteLine(Giftwrapper);
+            Console.ReadKey();
+            #endregion
+
+            #region Unboxing
+           
+            Box = (string)Giftwrapper;
+            
+            Console.WriteLine(Box);
+            Console.ReadKey();
+            #endregion
+
+        }
+    }
+}
